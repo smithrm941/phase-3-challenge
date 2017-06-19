fe5ccc841a4ed60046be18a91ffad7a818f03ec3
smithrm941 <smithrm941@gmail.com>
2017-06-19 11:24:23 -0700

    complete double number route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ app.get('/zero', function(req, res) {
   res.send("0");
 })
 
+app.get('/double/:number', function(req, res) {
+  let number = req.params.number;
+  res.send(number + " times 2 is " + number*2);
+})
+
 app.listen(3000, function(){
   console.log("Server is running on port 3000!")
 })
