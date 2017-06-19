e643ba7971e59dd62de430e5230767bdf4153ffa
smithrm941 <smithrm941@gmail.com>
2017-06-19 12:58:40 -0700

    complete subtract route, update folder name, update README

---
+++ part-1/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/zero', function(req, res) {
+  res.send("0");
+})
+
+app.get('/add', function(req, res) {
+  let sum = parseInt(req.query.a) + parseInt(req.query.b);
+  res.send("a + b = " + sum);
+});
+
+app.get('/subtract', function(req, res) {
+  let difference = parseInt(req.query.a) - parseInt(req.query.b);
+  res.send("a - b = " + difference);
+});
+
+app.get('/double/:number', function(req, res) {
+  let number = req.params.number;
+  res.send(number + " times 2 is " + number*2);
+})
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000!")
+})
