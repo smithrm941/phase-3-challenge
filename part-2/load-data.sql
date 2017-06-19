3a7d6c8b8f043112f29861314fab3405c81da4d9
smithrm941 <smithrm941@gmail.com>
2017-06-19 15:58:21 -0700

    queries to create items, shoppers, orders tables, WIP insert statements

--- part-2/load-data.sql
+++ part-2/load-data.sql
@@ -1,1 +1,31 @@
 COPY groceries.items (name, price, section) FROM 'full/path/to/groceries.csv' DELIMITER ',' CSV HEADER;
+
+INSERT INTO groceries.shoppers (shopper_lastname, shopper_firstname) VALUES
+  ('Smith', 'Rhonda'),
+  ('John', 'Public'),
+  ('Solomon', 'Tom'),
+  ('Solomon', 'Dick'),
+  ('Solomon', 'Harry'),
+  ('Solomon', 'Sally');
+
+INSERT INTO groceries.orders (shopper, order_date, item1, item2, item3, item4, item5) VALUES
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  (),
+  ();
--- part-2/schema.sql
+++ part-2/schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE grocery_store;
+
 CREATE DATABASE grocery_store;
 
 CREATE SCHEMA groceries;
@@ -9,10 +11,19 @@ CREATE TABLE groceries.items (
   section TEXT NOT NULL
 );
 
-CREATE TABLE grocery_store.shoppers (
-
+CREATE TABLE groceries.shoppers (
+  id SERIAL PRIMARY KEY,
+  shopper_lastname TEXT NOT NULL,
+  shopper_firstname TEXT NOT NULL
 );
 
-CREATE TABLE grocery_store.orders (
-
+CREATE TABLE groceries.orders (
+  id SERIAL PRIMARY KEY,
+  shopper INT REFERENCES groceries.shoppers(id) NOT NULL,
+  order_date DATE NOT NULL,
+  item1 INT REFERENCES groceries.items(id),
+  item2 INT REFERENCES groceries.items(id),
+  item3 INT REFERENCES groceries.items(id),
+  item4 INT REFERENCES groceries.items(id),
+  item5 INT REFERENCES groceries.items(id)
 );
