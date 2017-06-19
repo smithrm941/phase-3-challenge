59c2792b873cddf510ff8ccc55f34715159bc1c2
smithrm941 <smithrm941@gmail.com>
2017-06-19 12:21:53 -0700

    complete add route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ app.get('/zero', function(req, res) {
   res.send("0");
 })
 
+app.get('/add', function(req, res) {
+  let sum = parseInt(req.query.a) + parseInt(req.query.b);
+  res.send("a + b = " + sum);
+});
+
 app.get('/double/:number', function(req, res) {
   let number = req.params.number;
   res.send(number + " times 2 is " + number*2);
