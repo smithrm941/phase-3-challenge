ad9f104a95a8525db34027ce3b9820955141f783
smithrm941 <smithrm941@gmail.com>
2017-06-20 19:10:49 -0700

    complete grocery store page, update README

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,5 +1,3 @@
-console.log("it's working!")
-
 //Cart modal display:
 let cartModal = document.getElementById("cartModal");
 let cartButton = document.getElementById("cart-button")
@@ -8,9 +6,11 @@ cartButton.onclick = function() {
 }
 
 //Cart modal features:
+let totalCost = 0
+document.getElementById('total-cost').innerHTML = totalCost.toFixed(2);
+
+
 let itemsInCart = document.getElementById('list-items-in-cart');
-let totalCost = document.getElementById('total-cost');
-totalCost.innerHTML = 0;
 let closeModal = document.getElementById('close');
 let clearCart = document.getElementById('clear');
 
@@ -21,58 +21,70 @@ closeModal.onclick = function () {
 clearCart.onclick = function () {
   itemCount.innerHTML = 0
   itemsInCart.innerHTML = "";
+  totalCost = 0;
+  updateTotalCost();
 }
 
 
 //Add to Cart buttons functionality:
 let itemCount = document.getElementById("cart-item-count")
 itemCount.innerHTML = 0;
+function updateTotalCost () {
+  document.getElementById('total-cost').innerHTML = totalCost.toFixed(2);
+}
 
 let addOnions = document.getElementById("onion-button");
 addOnions.onclick = function() {
   itemCount.innerHTML ++
   itemsInCart.innerHTML += "<li>Onions $1.11</li>"
-  totalCost.innerHTML += 1.11
+  totalCost+= 1.11;
+  updateTotalCost();
 }
 
 let addTomatoes = document.getElementById("tomato-button");
 addTomatoes.onclick = function() {
   itemCount.innerHTML ++
   itemsInCart.innerHTML += "<li>Tomatoes $1.56</li>"
-  totalCost.innerHTML += 1.56
+  totalCost += 1.56;
+  updateTotalCost();
 }
 
 let addCarrots = document.getElementById("carrot-button");
 addCarrots.onclick = function() {
   itemCount.innerHTML ++
   itemsInCart.innerHTML += "<li>Carrots $2.88</li>"
-  totalCost.innerHTML += 2.88
+  totalCost += 2.88
+  updateTotalCost();
 }
 
 let addColdCuts = document.getElementById("cold-cut-button");
 addColdCuts.onclick = function() {
   itemCount.innerHTML ++
   itemsInCart.innerHTML += "<li>Cold Cuts $1.47</li>"
-  totalCost.innerHTML += 1.47
+  totalCost += 1.47
+  updateTotalCost();
 }
 
 let addOrangeJuice = document.getElementById("orange-juice-button");
 addOrangeJuice.onclick = function() {
   itemCount.innerHTML ++
   itemsInCart.innerHTML += "<li>Orange Juice $2.52</li>"
-  totalCost.innerHTML += 2.52
+  totalCost += 2.52
+  updateTotalCost();
 }
 
 let addIceCream = document.getElementById("ice-cream-button");
 addIceCream.onclick = function() {
   itemCount.innerHTML ++
   itemsInCart.innerHTML += "<li>Ice Cream $14.08</li>"
-  totalCost.innerHTML += 14.08
+  totalCost += 14.08
+  updateTotalCost();
 }
 
 let addPizza = document.getElementById("pizza-button");
 addPizza.onclick = function() {
   itemCount.innerHTML ++
   itemsInCart.innerHTML += "<li>Pizza $16.61</li>"
-  totalCost.innerHTML += 16.61
+  totalCost += 16.61
+  updateTotalCost();
 }
